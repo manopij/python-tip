e2be7597456097b2715b024184b98c669c4ece0e
manopij <manopij@gmail.com>
2021-11-23 22:47:19 +0700

    complete 05 dataset

--- 05_Data_Structure/dictionnaries.py
+++ 05_Data_Structure/dictionnaries.py
@@ -27,6 +27,13 @@ print(a)
 b=dict(sape=4139, guido=4127, jack=4098)
 print(b)
 
+# looping through dictionaries
+# the key and corresponding value can be retrieved at the same time using the items() method
+knights = {'gallahad': 'the pure', 'robin': 'the brave'}
+for k, v in knights.items():
+    print(k,v)
+
 #dict comprehensions can be used to create dictionaries from arbitrary key and value expressions
 a={x: x**2 for x in (2, 4, 6)}
 print(a)
+
